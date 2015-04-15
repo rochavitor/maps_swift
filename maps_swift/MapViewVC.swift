b1514ce8c80553936c287d6debd744c05c5b4582
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-04-15 17:39:30 -0300

    comentários do código pro maps

--- maps_swift/MapViewVC.swift
+++ maps_swift/MapViewVC.swift
@@ -11,27 +11,39 @@ import MapKit
 
 class MapViewVC: UIViewController, MKMapViewDelegate{
     
+    //cria um outlet que sai da lista de teatros pro mapa
     @IBOutlet weak var MapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
+        //variável de endereço, vai receber o valor do teatro selecionado
         var address = "RUA RODOLFO GOTTARDELLO, CAMPINAS, SAO PAULO, BRASIL"
+        //inicia um geocoder
         var geocoder = CLGeocoder()
-        var latdelta: CLLocationDegrees = 0.01
-        var longdelta: CLLocationDegrees = 0.01
+        //variáveis das restrições de distância do ponto inicial
+        let latdelta: CLLocationDegrees = 0.01
+        let longdelta: CLLocationDegrees = 0.01
         
-        var theSpan: MKCoordinateSpan = MKCoordinateSpanMake(latdelta,longdelta)
+        //variável que estabelece o tamanho da área do mapa a ser mostrada
+        let theSpan: MKCoordinateSpan = MKCoordinateSpanMake(latdelta,longdelta)
         
+        //função que transforma o endereço em string em coordenadas de longitude e latitude e sete o ponto com sua área no mapa
         geocoder.geocodeAddressString(address, completionHandler: {(placemarks: [AnyObject]!, error:NSError!) -> Void in
+            
             if let placemark = placemarks?[0] as? CLPlacemark{
                 
-                var location: CLLocation
+                var location: CLLocation //cria variável de localização que recebe a localização do placemark
                 location = placemark.location
-                var coordinate: CLLocationCoordinate2D
+                
+                var coordinate: CLLocationCoordinate2D //variável que recebe as coordenadas em 2D da localização
                 coordinate = location.coordinate
+                
+                //Variável que utiliza o ponto central e o tamanho da área a ser mostrada
+                //para definir a região do mapa que vai ser mostrada
                 var theRegion: MKCoordinateRegion = MKCoordinateRegionMake(coordinate, theSpan)
-                self.MapView.setRegion(theRegion, animated:true)
-                self.MapView.addAnnotation(MKPlacemark(placemark: placemark))
+                
+                self.MapView.setRegion(theRegion, animated:true) //defina a região no map view
+                self.MapView.addAnnotation(MKPlacemark(placemark: placemark)) //coloca o placemark no local selecionado
                 
                 
             }
