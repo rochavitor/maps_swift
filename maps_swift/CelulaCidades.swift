938046c55a95082551a513c61c774b962aabd19a
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-04-16 15:32:35 -0300

    célula selecionada permanecia selecionada, problema resolvido via comentário: a ser discutido

--- maps_swift/CelulaCidades.swift
+++ maps_swift/CelulaCidades.swift
@@ -17,7 +17,7 @@ class CelulaCidades: UITableViewCell {
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+       // super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
